201a7210fabac5e35a8c72f54f6e8ead75ede302
Blake Wisniewski <wisniewski.blake@gmail.com>
2021-02-12 01:26:47 -0500

    created file system

---
+++ GreenBeans/View/Main/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  GreenBeans
+//
+//  Created by Blake Wisniewski on 11/24/1399 AP.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
