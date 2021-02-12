450384345f4f1dd0a5cf309c14c569e41351dac3
Blake Wisniewski <wisniewski.blake@gmail.com>
2021-02-12 01:00:09 -0500

    Initial Commit

---
+++ GreenBeans/GreenBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenBeansApp.swift
+//  GreenBeans
+//
+//  Created by Blake Wisniewski on 11/24/1399 AP.
+//
+
+import SwiftUI
+
+@main
+struct GreenBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
