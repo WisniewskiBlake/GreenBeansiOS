040c48b79335724b0549d98bd158b21323171c85
Blake Wisniewski <wisniewski.blake@gmail.com>
2021-02-16 03:29:15 -0500

    side menu working perfectly

--- GreenBeans/View/Main/ContentView.swift
+++ GreenBeans/View/Main/ContentView.swift
@@ -17,33 +17,6 @@ struct ContentView: View {
     var body: some View {
         Group {
             if viewModel.userSession != nil {
-//                NavigationView {
-//                    ZStack {
-////                        if showingMenu {
-////                            SideMenuView(isShowing: $showingMenu)
-////                                .padding(.top, 44)
-////                                .ignoresSafeArea()
-////                        }
-//
-////                        StartOrderView()
-////                            .cornerRadius(showingMenu ? 20 : 10)
-////                            .ignoresSafeArea()
-////                            .navigationBarTitle(viewModel.tabTitle(forIndex: selectedIndex))
-////                            .navigationBarTitleDisplayMode(.inline)
-////                            .offset(x: showingMenu ? 300 : 0, y: showingMenu ? 44 : 0)
-////                            .opacity(showingMenu ? 0.25 : 1)
-////                            .scaleEffect(showingMenu ? 0.9 : 1)
-////                            .shadow(color: self.showingMenu ? .black : .clear, radius: 20, x: 0, y: 0)
-////                            .disabled(showingMenu)
-////                            .navigationBarItems(leading: menuButton,
-////                                                trailing: selectedIndex == 0 ? refreshButton : nil)
-//
-//
-//
-//                    }.onAppear(perform: {
-//                        self.showingMenu = false
-//                    })
-//                }
                 NavigationView {
                     ZStack {
                         if showingMenu {
@@ -52,27 +25,26 @@ struct ContentView: View {
                                 .ignoresSafeArea()
                         }
 
-                        MainNavView()
-                            
-    //
-    //
-                    }
-                    
+                        StartOrderView()
+                            .cornerRadius(showingMenu ? 20 : 10)
+                            .ignoresSafeArea()
+                            .navigationBarTitle("Green Beans")
+                            .navigationBarTitleDisplayMode(.inline)
+                            .offset(x: showingMenu ? 300 : 0, y: showingMenu ? 44 : 0)
+                            .opacity(showingMenu ? 0.25 : 1)
+                            .scaleEffect(showingMenu ? 0.9 : 1)
+                            .shadow(color: self.showingMenu ? .black : .clear, radius: 20, x: 0, y: 0)
+                            .disabled(showingMenu)
+                            .navigationBarItems(leading: menuButton,
+                                                trailing: selectedIndex == 0 ? refreshButton : nil)
+
+
+
+                    }.onAppear(perform: {
+                        self.showingMenu = false
+                    })
                 }
-                .cornerRadius(showingMenu ? 20 : 10)
-                .ignoresSafeArea()
-                .navigationBarTitle("Green Beans")
-                .navigationBarTitleDisplayMode(.inline)
-                .offset(x: showingMenu ? 300 : 0, y: showingMenu ? 44 : 0)
-                .opacity(showingMenu ? 0.25 : 1)
-                .scaleEffect(showingMenu ? 0.9 : 1)
-                .shadow(color: self.showingMenu ? .black : .clear, radius: 20, x: 0, y: 0)
-                .disabled(showingMenu)
-                .navigationBarItems(leading: menuButton,
-                                    trailing: selectedIndex == 0 ? refreshButton : nil)
-                .onAppear(perform: {
-                    self.showingMenu = false
-                })
+
                 
                 
                 
